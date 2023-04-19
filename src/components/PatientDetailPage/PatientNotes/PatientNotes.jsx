1fd218e293c29904e40fb39864fe09673ca1bb5a
helmuth <helmuth.hernandez.alcazar@alumnos.upm.es>
2023-04-19 16:58:53 +0200

    Patient notes with pagination

--- src/components/PatientDetailPage/PatientNotes/PatientNotes.jsx
+++ src/components/PatientDetailPage/PatientNotes/PatientNotes.jsx
@@ -1,6 +1,14 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import { Button, Col, Container, Row, Table } from "react-bootstrap";
+import {
+  Button,
+  Col,
+  Container,
+  Form,
+  Pagination,
+  Row,
+  Table,
+} from "react-bootstrap";
 import { set, useForm } from "react-hook-form";
 import { authenticationService } from "../../../services/authentication/authenticationService";
 import getNotes from "../../../services/patient/getNotes";
@@ -9,16 +17,50 @@ const PatientNotes = (props) => {
   const patientId = props.patientId;
   const [notes, setNotes] = useState([]);
   const [refreshToggle, setRefreshToggle] = useState(false);
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState(10);
+  const [sort, setSort] = useState("date,desc");
+  const [url, setUrl] = useState(
+    `http://localhost:8080/notes/search/findByPatient?patient=/patients/${patientId}&sort=${sort}`
+  );
+  const [links, setLinks] = useState({});
+  const [nextPageDisabled, setNextPageDisabled] = useState(false);
+  const [prevPageDisabled, setPrevPageDisabled] = useState(true);
+  /*
   useEffect(() => {
-    getNotes(patientId).then((notes) => setNotes(notes));
+    getNotes(patientId).then((notes) =>
+      setNotes([...notes].sort((a, b) => b.date.localeCompare(a.date)))
+    );
   }, [refreshToggle]);
+
+  */
+
+  useEffect(() => {
+    fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: authenticationService.getSessionToken(),
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const { size, totalPages, number } = data.page;
+        setNotes(data._embedded.notes);
+        setLinks(data._links);
+        data._links.next !== undefined
+          ? setNextPageDisabled(false)
+          : setNextPageDisabled(true);
+        data._links.prev !== undefined
+          ? setPrevPageDisabled(false)
+          : setPrevPageDisabled(true);
+      });
+  }, [refreshToggle, url]);
 
   const refreshTable = () => {
     setRefreshToggle(!refreshToggle);
   };
   return (
     <Container>
-      <h2>Notas del paciente</h2>
       <Table
         striped
         bordered
@@ -55,21 +97,42 @@ const PatientNotes = (props) => {
           })}
         </tbody>
       </Table>
-      <AddNoteSection patientId={props.patientId} refreshTable={refreshTable} />
+      <Pagination>
+        <Pagination.Prev
+          onClick={() => {
+            setUrl(links.prev.href);
+            console.log(links.prev.href);
+          }}
+          disabled={prevPageDisabled}
+        ></Pagination.Prev>
+        <Pagination.Next
+          onClick={() => {
+            setUrl(links.next.href);
+            console.log(links.next.href);
+          }}
+          disabled={nextPageDisabled}
+        ></Pagination.Next>
+      </Pagination>
+      <AddNoteSection
+        style={{ marginTop: "16px" }}
+        patientId={props.patientId}
+        refreshTable={refreshTable}
+      />
     </Container>
   );
 };
 
 const AddNoteSection = (props) => {
+  const style = props.style;
   const patientId = props.patientId;
   const [response, setResponse] = useState({});
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
 
   function onSubmit(data) {
     console.log(JSON.stringify(data, null, 2));
     const body = {
       date: new Date(),
-      note: data.note,
+      note: data.note.trim(),
       patient: "/patients/" + patientId,
     };
 
@@ -87,27 +150,30 @@ const AddNoteSection = (props) => {
       console.log(response);
     });
 
-    //window.location.reload(false);
+    reset();
   }
   return (
-    <Container>
-      <form
-        className="form-group shadow p-3 mb-5 bg-body rounded"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div>
-          <textarea cols={100} {...register("note")}></textarea>
-        </div>
+    <Form
+      style={style}
+      className="form-group shadow p-3 mb-5 bg-body rounded"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <h3>Nueva nota</h3>
 
-        <div>
-          <input
-            type="submit"
-            value="Añadir nota"
-            className="btn btn-primary"
-          ></input>
-        </div>
-      </form>
-    </Container>
+      <Form.Group>
+        <Form.Control
+          as="textarea"
+          cols={130}
+          {...register("note")}
+        ></Form.Control>
+      </Form.Group>
+
+      <Form.Group style={{ marginTop: "8px" }}>
+        <Button className="float-right" type="submit" variant="primary">
+          Añadir
+        </Button>
+      </Form.Group>
+    </Form>
   );
 };
 
