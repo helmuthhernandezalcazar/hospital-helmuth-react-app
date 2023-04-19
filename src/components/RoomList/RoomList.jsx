b2d330174e3b8783ccf5b451aadfa9bca02fdd18
helmuth <helmuth.hernandez.alcazar@alumnos.upm.es>
2023-04-19 10:09:53 +0200

    Roomlist fetchs with authorization

--- src/components/RoomList/RoomList.jsx
+++ src/components/RoomList/RoomList.jsx
@@ -6,6 +6,8 @@ import { useParams } from "react-router-dom";
 import getRooms from "../../services/medicalFloor/getRooms";
 import { medicalFloorService } from "../../services/medicalFloor/medicalFloorService";
 import RoomCard from "../RoomCard";
+import getMedicalSpecialty from "../../services/medicalFloor/getMedicalSpecialty";
+import { authenticationService } from "../../services/authentication/authenticationService";
 
 const RoomList = () => {
   const { id } = useParams([]);
@@ -13,37 +15,38 @@ const RoomList = () => {
   const [medicalFloor, setMedicalFloor] = useState({});
   const [medicalSpecialty, setMedicalSpecialty] = useState({});
   useEffect(() => {
-    console.log(id);
-
-    medicalFloorService.getRooms(id).then((rooms) => setRooms(rooms));
-  }, []);
-
-  useEffect(() => {
-    console.log(id);
-    medicalFloorService
-      .getById(id)
+    fetch("http://localhost:8080/medicalFloors/" + id, {
+      method: "GET",
+      headers: {
+        Authorization: authenticationService.getSessionToken(),
+      },
+    })
+      .then((response) => response.json())
       .then((medicalFloor) => setMedicalFloor(medicalFloor));
-  }, []);
+    getMedicalSpecialty(id).then((medicalSpecialty) =>
+      setMedicalSpecialty(medicalSpecialty)
+    );
+    getRooms(id).then((rooms) => setRooms(rooms));
+  }, [id]);
 
   return (
     <>
-      {rooms.length > 0 ? (
-        <Container>
-          <Row className="row-cols-4">
-            {rooms.map((room, index) => {
-              const self = room._links.self.href;
-              const roomId = self.split("http://localhost:8080/rooms/").pop();
-              return (
-                <Col key={index}>
-                  <RoomCard room={room} roomId={roomId} />
-                </Col>
-              );
-            })}
-          </Row>
-        </Container>
-      ) : (
-        <p>loading</p>
-      )}
+      <Container>
+        <h1>
+          {medicalFloor.name} {medicalSpecialty.name}
+        </h1>
+        <Row className="row-cols-4">
+          {rooms.map((room, index) => {
+            const self = room._links.self.href;
+            const roomId = self.split("http://localhost:8080/rooms/").pop();
+            return (
+              <Col key={index}>
+                <RoomCard room={room} roomId={roomId} />
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </>
   );
 };
--- src/services/medicalFloor/getRooms.js
+++ src/services/medicalFloor/getRooms.js
@@ -1,5 +1,12 @@
+import { authenticationService } from "../authentication/authenticationService";
+
 export default function getRooms(medicalFloorId) {
-  return fetch(`http://localhost:8080/medicalFloors/${medicalFloorId}/rooms`)
+  return fetch(`http://localhost:8080/medicalFloors/${medicalFloorId}/rooms`, {
+    method: "GET",
+    headers: {
+      Authorization: authenticationService.getSessionToken(),
+    },
+  })
     .then((response) => response.json())
     .then((data) => {
       return data._embedded.rooms;
